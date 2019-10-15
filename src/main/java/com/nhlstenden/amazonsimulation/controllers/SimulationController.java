d7cea242a3eb33edca85093435994df907e5e7f6
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-10-15 20:16:27 +0200

    Update Simulation code

--- src/main/java/com/nhlstenden/amazonsimulation/controllers/SimulationController.java
+++ src/main/java/com/nhlstenden/amazonsimulation/controllers/SimulationController.java
@@ -1,17 +1,29 @@
 package com.nhlstenden.amazonsimulation.controllers;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import com.nhlstenden.amazonsimulation.models.Simulation;
+
 @Controller
-public class SimulationController {
+public class SimulationController implements Observer {
 
-	private SimpMessagingTemplate messagingTemplate;
-
+	private SimpMessagingTemplate messagingService;
+	
+	private Simulation simulation;
+	
 	@Autowired
-	public SimulationController(SimpMessagingTemplate messagingTemplate) {
-		this.messagingTemplate = messagingTemplate;
+	public SimulationController(SimpMessagingTemplate messagingService) {
+		this.messagingService = messagingService;
+	}
+
+	@Override
+	public void update(Observable simulation, Object event) {
+		
 	}
 	
 }
---
+++ src/main/java/com/nhlstenden/amazonsimulation/models/Simulation.java
@@ -0,0 +1,7 @@
+package com.nhlstenden.amazonsimulation.models;
+
+import java.util.Observable;
+
+public class Simulation extends Observable {
+	
+}
