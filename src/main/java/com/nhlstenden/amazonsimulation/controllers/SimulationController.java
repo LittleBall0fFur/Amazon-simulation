697412cb4dd48ea2ae6d45ed0baea25fc3bda579
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-10-16 20:17:32 +0200

    Add PropertyChangeSupport and Example Comments

--- src/main/java/com/nhlstenden/amazonsimulation/controllers/SimulationController.java
+++ src/main/java/com/nhlstenden/amazonsimulation/controllers/SimulationController.java
@@ -16,6 +16,24 @@ public class SimulationController {
 	@Autowired
 	public SimulationController(SimpMessagingTemplate messagingService) {
 		this.messagingService = messagingService;
+		
+		this.simulation = new Simulation(/* PASS PARAMETERS LIKE 'AMOUNT OF ROBOTS' ETC */);
+		
+		/* ADD LISTENERS TO SIMULATION */
+		//	EXAMPLE:
+		//	this.simulation.addPropertyChangeListener(Simulation.ListenableProperty.ROBOT_POSITION, (event) -> {
+		// 		sendRobotPositionToClient((Vector3D) event.getNewValue()); //Convert event to expected object and pass to handling function.
+		// 	});
+		//
+		// 	Repeat for other properties.
+		
+		/* START SIMULATION ON ANOTHER THREAD */
+		//	EXAMPLE: this.simulation.start();
 	}
+	
+	//	EXAMPLE:
+	//	private void sendRobotPositionToClient(Vector3D new_robotPosition) {
+	//		this.messagingService.convertAndSend("topic/robot/position", new_robotPosition); //Send position to client.
+	//	}
 	
 }
--- src/main/java/com/nhlstenden/amazonsimulation/models/Simulation.java
+++ src/main/java/com/nhlstenden/amazonsimulation/models/Simulation.java
@@ -1,5 +1,26 @@
 package com.nhlstenden.amazonsimulation.models;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class Simulation {
+	
+	public enum ListenableProperty {
+		//	EXAMPLE: ROBOT_POSITION, ROBOT_ISLOADED, etc.
+	}
+	
+	private final PropertyChangeSupport eventService;
+	
+	public Simulation() {
+		this.eventService = new PropertyChangeSupport(this);
+	}
+	
+	public void addPropertyChangeListener(ListenableProperty property, PropertyChangeListener listener) {
+		this.eventService.addPropertyChangeListener(property.toString(), listener);
+	}
+	
+	public void removeListener(ListenableProperty property, PropertyChangeListener listener) {
+		this.eventService.removePropertyChangeListener(property.toString(), listener);
+	}
 	
 }
