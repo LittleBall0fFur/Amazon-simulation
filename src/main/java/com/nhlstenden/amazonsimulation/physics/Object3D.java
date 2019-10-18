9c49ff081fd7a0ade4944f6d8b849d3759610f17
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-10-18 17:57:02 +0200

    Change class 'Object3D'

--- src/main/java/com/nhlstenden/amazonsimulation/physics/Object3D.java
+++ src/main/java/com/nhlstenden/amazonsimulation/physics/Object3D.java
@@ -3,19 +3,29 @@ package com.nhlstenden.amazonsimulation.physics;
 public abstract class Object3D {
 
 	private Vector3D position;
-	
 	private Vector3D velocity;
 	private Vector3D acceleration;
 	
-	public Object3D() {
+	protected Object3D() {
 		this.position = new Vector3D();
-		
 		this.velocity = new Vector3D();
 		this.acceleration = new Vector3D();
 	}
 	
-	public Object3D(Vector3D position) {
+	protected Object3D(Vector3D position) {
+		this.position = position;
+		this.velocity = new Vector3D();
+		this.acceleration = new Vector3D();
+	}
+	
+	protected Object3D(final Object3D other) {
+		this(other.position, other.velocity, other.acceleration);
+	}
+	
+	protected Object3D(Vector3D position, Vector3D velocity, Vector3D acceleration) {
 		this.position = new Vector3D(position);
+		this.velocity = new Vector3D(velocity);
+		this.acceleration = new Vector3D(acceleration);
 	}
 	
 	public void update() {
